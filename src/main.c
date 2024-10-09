deddebb73548e2c1c18d060f007db8a79be17fdc
Lawrence <6730017+lawrence97@users.noreply.github.com>
2024-10-09 04:08:21 +0100

    window and glad

--- src/main.c
+++ src/main.c
@@ -2,4 +2,48 @@
 #include <GLFW/glfw3.h>
 #include <glad/glad.h>
 
-int main() { return 0; }
+#include <stdio.h>
+
+#define WIDTH 640
+#define HEIGHT 640
+
+int main() {
+
+	int width = WIDTH;
+	int height = HEIGHT;
+
+	if (glfwInit() == 0) {
+		printf("error - glfw stage.\n");
+		return -1;
+	}
+
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+	glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+	GLFWwindow *window = glfwCreateWindow(width, height, "glfw", NULL, NULL);
+	if (window == NULL) {
+		printf("error - window stage.\n");
+		return -1;
+	}
+
+	glfwMakeContextCurrent(window);
+	if (gladLoadGLLoader((GLADloadproc)glfwGetProcAddress) == 0) {
+		glfwTerminate();
+		printf("error - glad stage.\n");
+		return -1;
+	}
+
+	glfwSwapInterval(1);
+	glViewport(0, 0, width, height);
+	glClearColor(.1f, .1f, .1f, 1.0f);
+
+	while (!glfwWindowShouldClose(window)) {
+		glClear(GL_COLOR_BUFFER_BIT);
+		glfwPollEvents();
+		glfwSwapBuffers(window);
+	}
+
+	glfwDestroyWindow(window);
+
+	return 0;
+}
